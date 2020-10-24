513cc8d13e0d0943dd08376ef8cd15510975eaca
Maitre-Pangolin <martin.wasselet@gmail.com>
2020-10-24 15:35:06 -0400

    Added correct T piece Added last hole validation method

--- GameSolver.py
+++ GameSolver.py
@@ -1,6 +1,6 @@
 #possiblePosition=[[1,1],[6,1],[3,2],[2,3],[6,3],[4,4],[1,5],[3,5],[6,5],[5,6]] # Grid 1
 #possiblePosition=[[5,1],[2,2],[4,2],[1,3],[2,3],[5,3],[4,4],[5,5],[1,6],[4,6]] # Grid 2
-#possiblePosition=[[2,1],[5,1],[4,2],[1,3],[3,3],[6,3],[2,4],[1,5],[3,5],[5,5]] # Grid 3
+possiblePosition=[[2,1],[5,1],[4,2],[1,3],[3,3],[6,3],[2,4],[1,5],[3,5],[5,5]] # Grid 3
 #possiblePosition=[[3,1],[6,1],[2,3],[4,3],[1,4],[3,4],[6,4],[5,5],[2,6],[4,6]] # Grid 4
 #possiblePosition=[[1,1],[5,1],[3,2],[1,3],[2,4],[4,4],[6,4],[3,5],[5,5],[4,6]] # Grid 5 WORKING 
 #possiblePosition=[[1,1],[6,1],[2,2],[4,3],[2,4],[5,4],[1,5],[4,5],[6,5],[3,6]] # Grid 6  
@@ -9,7 +9,7 @@
 #possiblePosition=[[5,1],[2,2],[1,3],[5,3],[6,4],[2,5],[4,5],[1,6],[3,6],[5,6]] # Grid 9
 #possiblePosition=[[1,1],[3,1],[5,1],[2,3],[4,3],[6,3],[3,4],[4,4],[1,6],[4,6]] # Grid 10
 #possiblePosition=[[1,1],[5,1],[1,3],[5,3],[3,4],[6,4],[1,5],[4,5],[2,6],[5,6]] # Grid 11
-possiblePosition=[[5,1],[3,2],[2,3],[5,3],[2,4],[4,4],[6,4],[1,5],[3,5],[5,5]] # Grid 12
+#possiblePosition=[[5,1],[3,2],[2,3],[5,3],[2,4],[4,4],[6,4],[1,5],[3,5],[5,5]] # Grid 12
 #possiblePosition=[[2,1],[6,1],[1,2],[5,2],[2,4],[6,4],[4,5],[1,6],[3,6],[5,6]] # Grid 19
 
 #possiblePosition=[[x+1,y+1] for x in range(6) for y in range(6)] # all pos
@@ -25,12 +25,13 @@ L2=[[0,-1],[0,-2],[1,-2]]
 L3=[[1,0],[0,-1],[0,-2]]
 L4=[[0,1],[0,-1],[1,-1]]
 T1=[[-1,0],[-2,0],[-1,1]]
-T2=T1.copy()
+#T2=T1.copy() # Wrong T2 piece (what we physically have)
+T2=[[0,1],[-1,1],[1,1]] # Correct T2 piece to solve the game (what we should have)
 S1=[[0,1],[-1,1],[-1,2]]
 S2=[[0,-1],[-1,-0],[-1,1]]
 S3=S1.copy() # Other hole in -1 1 
-S3other=S2.copy() # Other hole in -1 1
-ValidHole=[-1,1] ######################### WIP
+S3other=S2.copy() # Other hole in -1 2
+validationHole=[[-1,1] ,[-1,2]]
 shapes=[L1,L2,L3,L4,T1,T2,S1,S2,S3]
 
 # Create all pieceStates orientation from shapes
@@ -58,6 +59,18 @@ S3OtherOrientation.append([
          [ [-S3other[i][1],-S3other[i][0]] for i in range(3)] , # -Y-X
          [ [S3other[i][1],-S3other[i][0]] for i in range(3)]   #  Y -X
     ])
+validationHoleOrientation=[]
+for config in validationHole:
+     validationHoleOrientation.append([
+            [config[0],config[1]]    , # X Y 
+            [-config[0],config[1]]  ,  # -X Y
+            [-config[0],-config[1]] , # -X-Y
+            [config[0],-config[1]]  , # X -Y
+            [config[1],config[0]]   , # Y X
+            [-config[1],config[0]]  ,  #  -Y X
+            [-config[1],-config[0]] , # -Y-X
+            [config[1],-config[0]]   #  Y -X
+    ])
 
 def printPiece(piece_num,orientation_num):
     indexToFill=[]
@@ -80,8 +93,6 @@ def printPiece(piece_num,orientation_num):
         print(line) 
     print("\n") 
 
-### Define wallClearance and faceClearance function to determine if pieceStates state are acceptable or not ( using index in arguments rather than full lists)
-
 def wallClearance(pos_num,orientation_num,piece_num):
     for square in shapeOrientations[piece_num][orientation_num]:
         if square[0]+possiblePosition[pos_num][0] > 6 or square[0]+possiblePosition[pos_num][0] <1 or square[1]+possiblePosition[pos_num][1] > 6 or square[1]+possiblePosition[pos_num][1] < 1:
@@ -95,7 +106,7 @@ def faceClearance(pos_num,orientation_num,piece_num):
                 if piece_num ==8 and square == shapeOrientations[piece_num][orientation_num][2]:
                     return True
                 else:
-                return False
+                    return False
     return True
 
 def boardStateValidation(combinedStates):
@@ -140,6 +151,17 @@ def printBoard(combinedStates):
     for line in board[::-1]:
         print(line)
     print("\n")
+
+def boardStateLastHoleClearance(boardState,config):
+    lastPiecePos=boardState[8][0]
+    lastPieceOri=boardState[8][1]
+    secondHolePos=[possiblePosition[lastPiecePos][0] + validationHoleOrientation[config][lastPieceOri][0],possiblePosition[lastPiecePos][1] + validationHoleOrientation[config][lastPieceOri][1]]
+    for pos in possiblePosition:
+        if secondHolePos[0]==pos[0] and secondHolePos[1]==pos[1]:
+            return True
+        else:
+            return False
+
 # Create all possible state for a piece (position , orientation)
 
 statePossible = [[(pos,ori) for pos in range(len(possiblePosition)) for ori in range(len(shapeOrientations[0])) if wallClearance(pos,ori,piece) and faceClearance(pos,ori,piece)] for piece in range(len(shapeOrientations))] # In love with comprehension list 
@@ -155,7 +177,12 @@ for pieceStates in statePossible[1:]:
     fullTable = list(filter(boardStateValidation,temp))
 
 print("number of combination for first S piece configuration: :",len(fullTable))
-for state in fullTable[:]:
+
+for state in fullTable:
+    if not boardStateLastHoleClearance(state,0):
+        fullTable.remove
+
+for state in fullTable:
     printBoard(state)
 
 shapeOrientations[8]=S3OtherOrientation[0]
@@ -166,9 +193,13 @@ fullTable = [[firstPieceStates] for firstPieceStates in statePossible[0]]
 for pieceStates in statePossible[1:9]:
     temp = []
     for combinedState in fullTable:
-        for states in pieceStates:
+        for states in pieceStates: 
             temp.append(combinedState+[states])
     fullTable = list(filter(boardStateValidation,temp))
+
+for state in fullTable:
+    if not boardStateLastHoleClearance(state,1):
+        fullTable.remove(state)
 
 print("number of combination for second S piece configuration:",len(fullTable))
 for state in fullTable[:]:
